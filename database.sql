cd0d480837cffac97091c55ebff8e1b748cb0403
Joonas Kelahaara <jolle.kela@gmail.com>
2021-11-14 17:39:48 +0200

    korjatumpi sql versio

--- database.sql
+++ database.sql
@@ -51,6 +51,6 @@ insert into category(name) value ('Tietokoneet');
 insert into category(name) value ('Puhelimet');
 insert into category(name) value ('Kodinkoneet');
 
-insert into product(name, price, image, category_id) values ('Läppäri', 999,99, 'placeholder.png', 1);
-insert into product(name, price, image, category_id) values ('iPhone', 1999,99, 'placeholder.png', 2);
-insert into product(name, price, image, category_id) values ('Mikroaaltouuni', 99,99 , 'placeholder.png', 3);
+insert into product(name, price, image, category_id) values ('Läppäri', 999, 'placeholder.png', 1);
+insert into product(name, price, image, category_id) values ('iPhone', 1999, 'placeholder.png', 2);
+insert into product(name, price, image, category_id) values ('Mikroaaltouuni', 99, 'placeholder.png', 3);

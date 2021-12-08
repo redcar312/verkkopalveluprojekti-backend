6a66ee320ae06c1c5680f6b371aafd1708432a8d
Joonas Kelahaara <jolle.kela@gmail.com>
2021-12-08 18:30:21 +0200

    search.php muutoksia

--- products/search.php
+++ products/search.php
@@ -7,7 +7,7 @@ $search = $_GET['name'];
 try {
     $db = getDbConnection();
 
-    selectAsJson($db, "SELECT name, price, image FROM product WHERE name = '$search'");
+    selectAsJson($db, "SELECT name, price, image FROM product WHERE name LIKE '%$search%'");
 
 } catch(PDOException $pdoex) {
     returnError($pdoex);

2327c26c98510aabdcfb33fe77ef089cc90c92f6
Joonas Kelahaara <jolle.kela@gmail.com>
2021-12-19 17:03:28 +0200

    muutosta

--- login/login.php
+++ login/login.php
@@ -5,8 +5,8 @@ require_once '../functions.php';
 
 if( isset($_SERVER['PHP_AUTH_USER']) ){
     if(checkUser(getDbConnection(), $_SERVER['PHP_AUTH_USER'],$_SERVER["PHP_AUTH_PW"] )){
-        $_SESSION["user"] = $_SERVER['PHP_AUTH_USER'];
-        {"info":"Kirjauduit sisään"}
+      $_SESSION["user"] = $_SERVER['PHP_AUTH_USER'];
+      echo '{"info":"Kirjauduit sisään"}';
       json_encode(array("info"=>"Kirjauduit sisään" ));
 
       echo '{"info":"Kirjauduit sisään"}';
